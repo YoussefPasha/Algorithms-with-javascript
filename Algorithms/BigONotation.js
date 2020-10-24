60248900dd531592f871397a146554768b85622e
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-10-24 23:04:18 +0200

    Timing Code

--- Algorithms/BigONotation.js
+++ Algorithms/BigONotation.js
@@ -1,5 +1,38 @@
+//First Solution Counter
+
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 0; i < n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+// Second Solution Counter
+
+function addUpTo2(n) {
+  return (n * (n + 1)) / 2;
+}
+
+function TestPerformance() {
+  // First Solution run
+  //check time with function performance
+  let t1 = performance.now();
+  addUpTo(100000000);
+  let t2 = performance.now();
+  console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`);
+  /** Thats what i got in First one 0.08559500001138076 seconds */
+  // Second Solution run
+  //check time with function performance
+  let t1 = performance.now();
+  addUpTo2(100000000);
+  let t2 = performance.now();
+  console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`);
+  /** Thats what i got in second 0.00001999997766688466 seconds away smaller */
+}
+
 function RUN() {
-  console.log("Run");
+  TestPerformance();
 }
 
 runBtn.addEventListener("click", RUN);
